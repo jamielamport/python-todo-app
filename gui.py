6dade41ceea5c991a0e96b8df20bdf984aa6c52b
Jamie Lamport <jamie.lamport@gmail.com>
2023-06-15 11:58:08 +0100

    Updating todo values and updatign the GUI when todos are selected

--- gui.py
+++ gui.py
@@ -5,8 +5,13 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
+
+
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -19,6 +24,20 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Edit":
+            todo_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
