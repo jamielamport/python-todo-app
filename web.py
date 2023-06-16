9a2bdcdf193f8d0fa4a06526bcf3c67092d31fc5
Jamie Lamport <jamie.lamport@gmail.com>
2023-06-16 10:11:26 +0100

    Ability to add todos implemented

--- web.py
+++ web.py
@@ -2,6 +2,14 @@ import streamlit as st
 from modules import functions
 import os
 
+todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state['new_todo'] = ''
+
+
 FILEPATH = "todos.txt"
 
 if not os.path.exists(FILEPATH):
@@ -17,7 +25,8 @@ todos = functions.get_todos()
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='', placeholder="Add new todo...")
+st.text_input(label='', placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 
-
+st.session_state
